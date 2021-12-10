7f4a4b156e99c1c413bf971eec49980fdfc771b1
AxiixAAA <slavae22@mail.ru>
2021-12-10 22:58:21 +0500

    sss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Navbar } from './components/Navbar';
+import { Alert } from './components/Alert';
+// import { AlertState } from './context/alert/AlertState';
 
 function App() {
-  return (
-    <div className="container">
-        <h1>Test</h1>
-    </div>
-  );
-}
+    return (
+    //   <AlertState>
+      <Router>
+          <Navbar />
+        <div className="container pt-4">
+            <Alert />
+          <Routes>
+            <Route path={'/'} exact element={<Home/>} />
+            <Route path={'/about'} element={<About/>} />
+          </Routes>
+        </div>
+      </Router>
+    //   </AlertState>
+    
+    );
+  }
 
 export default App;
